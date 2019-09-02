33ab6f4b67c8b86abd779aee498a8d4b9997f12e
musyoka8 <nagilmusyoka@gmail.com>
2019-09-02 16:28:03 +0300

    the end of the if function for size,quantity,order and price

--- js/script.js
+++ js/script.js
@@ -16,10 +16,45 @@ $(document).ready(function (){
         return this.sizeOfPizza + "," + this.quantityOfPizza +"," +this.crustOfPizza +"," + this.toppingsOfPizza + "," + this.deliveryOfPizza + ",";
     }
     $(document).ready(function(){
-        $("form#odd").submit(function (event){
-            event.preventDeafult();
+        $('form#odd').submit(function (event){
+            event.preventDefault();
             var newOrder;
-            
-        }
+            if ($("#Size").val()){
+                var inputtedSizeOfPizza = $('#Size').val() + "" + "price=1000frw";
+                console.log(inputtedSizeOfPizza)
+            }
+            else if ($("#Size1").val()) {
+                var inputtedSizeOfPizza = $('#Size1').val() + "" + "price=1500frw";
+                console.log(inputtedSizeOfPizza)
+            }
+            else if ($("#size3").val()) {
+                var inputtedSizeofpizza = $("#Size2").val() + "" + "price=2000frw";
+                console.log(inputtedSizeofpizza)
+            }
+            if ($('#Crust').val()) {
+                var inputtedCrustofpizza = $("#Crust").val() + "" + "price=1000frw";
+                console.log(inputtedCrustOfPizza)
+            }
+            else if ($('#Crust1').val()) {
+                var inputtedCrustOfPizza = $("#Crust1").val() + "" + "price=1500frw";
+                console.log(inputtedCrustOfPizza)
+            }
+            else if ($('#Crust2').val()) {
+                var inputtedCrustofPizza = $("#Crust3").val() + "" + "price=2000frw";
+                console.log(inputtedCrustOfPizza)
+            }
+            else if ($('#quantity').val()) {
+                var inputtedquantityofpizza = $("#quantity").val();
+                console.log(inputtedQuantityOfPizza)
+            }
+            else if ($("#quantity1").val()) {
+                var inputtedQuantityOfPizza = $('#quantity1').val();
+                console.log(inputtedQuantityOfPizza)
+            }
+            else if ($("#quantity2").val()) {
+                var inputtedQuantityofpizza = $('#quantity2').val();
+                console.log(inputtedQuantityOfPizza)
+            }
+            var
     })
 })
