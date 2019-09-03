7a6307a54782c73f72a5c28e4e55326cf482ebd5
musyoka8 <nagilmusyoka@gmail.com>
2019-09-03 11:11:10 +0300

    add arrays for the click function with the fade in and fade out effect

--- js/scrip2.js
+++ js/scrip2.js
@@ -45,4 +45,56 @@ $(document).ready(function () {
         $("#two5").fadeOut();
         $("#two6").fadeOut();
         $("#ask").fadeIn();
-        
+    });
+    $("#nn4").click(function () {
+        $("#two5").toggle();
+        $("#Bk").fadeIn();
+        $("#hide").fadeOut();
+        $("#two4").fadeOut();
+        $("#two2").fadeOut();
+        $("#two3").fadeOut();
+        $("#two1").fadeOut();
+        $("#two6").fadeOut(); 
+        $("#ask").fadeIn();
+
+    });
+    $("#nn5").click(function () {
+        $("#two6").toggle();
+        $("#Bk").fadeIn();
+        $("#hide").fadeOut();
+        $("#two1").fadeOut();
+        $("#two2").fadeOut();
+        $("#two3").fadeOut();
+        $("#two4").fadeOut();
+        $("#two5").fadeOut();
+        $("#ask").fadeIn();
+
+    });
+    $("#nn6").click(function () {
+        $("#Bk").fadeIn();
+        $("#ask").fadeIn();
+        $("#hide").fadeOut();
+        $("#two6").fadeOut();
+        $("#two1").fadeOut();
+        $("#two3").fadeOut();
+        $("#two4").fadeOut();
+        $("#two5").fadeOut();
+        $("#two2").fadeOut();
+
+    });
+    $("#Bk").click(function () {
+        $(".nn").click(function () {
+            $("#hide").show();
+            $("#two1").show();
+        });
+        $("#hide").toggle();
+        $("#two1").fadeOut();
+        $("#two2").fadeOut();
+        $("#two3").fadeOut();
+        $("#two4").fadeOut();
+        $("#two5").fadeOut();
+        $("#two6").fadeOut();
+        $("#BK").fadeOut();
+        $("#ask").fadeOut();
+        $("#hide2").fadeOut();
+        $("#hide3").fadeOut();
