30dda8d969a18576860ae919c61eba9b851500ea
musyoka8 <nagilmusyoka@gmail.com>
2019-09-03 11:20:47 +0300

    make final changes in the scrip2.js

--- js/scrip2.js
+++ js/scrip2.js
@@ -98,3 +98,49 @@ $(document).ready(function () {
         $("#ask").fadeOut();
         $("#hide2").fadeOut();
         $("#hide3").fadeOut();
+    });
+    $("#ask").click(function () {
+        alert("IF YOU WANT THE PIZZA TO BE DELIVERED FILL THAT FORM ON TOP RIGHT SIDE OF THE PAGE");
+    });
+
+    $("#order").click(function () {
+        alert("Your order will be delivered to your location");
+    });
+    $("#an").click(function () {
+        $("#hide2").fadeIn();
+    });
+    $("#an").click(function () {
+        $("#hide3").fadeIn();
+    });
+    $(".view").click(function () {
+        $("#for").fadeIn();
+        if ($("Size").val() !== "" && $("#Crust").val() !== "" && $("#quantity").val() !== "") {
+            var showalll = "Your ORDER: " + " TOPPING: " + $('input[type=checkbox]:checked').val() + ",SIZE: " + $("#Size").val() + ", CRUST : " + $("#Crust").val() + ",QUANTITY: " + $("#quantity").val();
+           
+            $("#viewall").text(showalll);
+        } else if ($("#Size1").val() !== "" && $("#Crust1").val() !== "" && $("#quantity1").val() !== "") {
+            var showalll = "Your ORDER: " + " TOPPING: " + $('input[type=checkbox]:checked').val() + ",SIZE" + $("#Size1").val() + ", CRUST : " + $("#Crust1").val() + ",Quantity: " + $("#quantity1").val();
+          
+            $("#viewall").text(showalll);
+        } else if ($("#Size2").val() !== "" && $("#Crust2").val() !== "" && $("#quantity2").val() !== "") {
+            var showalll = "Your ORDER: " + " TOPPING: " + $('input[type=checkbox]:checked').val() + "," + $("#Size2").val() + ",CRUST : " + $("#Crust2").val() + ",QUANTITY: " + $("#quantity2").val();
+           
+            $("#viewall").text(showalll);
+        } else {
+            var showalll = "Enter your choice";
+            $("#viewall").text(showalll);
+        }
+        if ($(".size").val() === "Large" && $("#Crust").val() == "Crispy") {
+            var price = "The Price is: " + "," + $(".quantity").val() * 1000 + "$";
+            $("#price").text(price);
+        } else if ($(".size").val() === "Medium") {
+            var price = "The price is: " + "," + $(".quantity").val() * 900 + "$";
+            $("#price").text(price);
+        } else {
+            var price = "The price is: " + "," + $(".quantity").val() * 500 + "$";
+            $("#price").text(price);
+        }
+        $("#viewall").text(showalll);
+    });
+});
+
