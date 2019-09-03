3cc2a4f64141b0e89978f8485dd06ea50ee0902c
musyoka8 <nagilmusyoka@gmail.com>
2019-09-03 09:53:35 +0300

    make final changes on script.js

--- js/script.js
+++ js/script.js
@@ -91,6 +91,129 @@ $(document).ready(function (){
                     console.log(additionToppings)
                     return additionToppings;
                 }
-            
-    })
-})
+                Toppings(thechoosenToppings)
+                if ($('#delivery').val() === "yes")
+                    var inputtedDeliveryofpizza = $('#delivery1').val() + "" + "delivery charged=250frw";
+                console.log(inputtedDeliveryOfpizza)
+            }
+            if ($('#delivery').val() === "no")
+                var inputtedDeliveryOfpizza = $('#delivery').val();
+            console.log(inputtedDeliveryofpizza)
+        });
+        var newOrder = new order(inputtedSizeOfPizza, inputtedQuantityOfPizza, inputtedCrustOfPizza, inputtedDeliveryOfPizza, additionToppings);
+        console.log(newOrder)
+        console.log(theChoosenToppings)
+        $('ul#show').append("<li><span>" + newOrder.allInformation() + "</span></li>");
+        $(".view").click(function () {
+            $("#viewall").show();
+            $("#psize").text(newOrder.sizeOfPizza);
+            $("#pcrust").text(newOrder.crustOfPizza);
+            $("#pquantity").text(newOrder.quantityOfPizza);
+            $("#delivery").text(newOrder.deliveryOfPizza);
+        });
+    });
+    function order(Size, Quantity, Crust, Toppings, Delivery) {
+        this.sizeOfPizza = Size;
+        this.quantityOfPizza = Quantity;
+        this.crustOfPizza = Crust;
+        this.toppingsOfPizza = Toppings;
+        this.deliveryOfPizza = Delivery;
+
+    }
+
+    order.prototype.allInformation = function () {
+        return this.sizeOfPizza + "," + this.quantityOfPizza + "," + this.crustOfPizza + "," + this.toppingsOfPizza + "," + this.deliveryOfPizza;
+    }
+
+    $('form#odd').submit(function (event) {
+        event.preventDefault();
+        var showalll = $("#size").val() + "," + $("#size1").val() + "," + $("#Size2").val() + "," + $("#crust").val() + "," + $("#crust1").val() + "," + $("#quantite").val();
+        +"," + $("#quantite1").val() + "," + $("#quantite2").val() + "," + $("#derivery").val() + "," + $('input[type=checkbox]:checked').value();
+        $("#viewall").text(showalll);
+        var newOrder;
+        if ($('#size').val()) {
+            var inputtedsizeofpizza = $('#size').val() + "" + "price=2000frw";
+            console.log(inputtedsizeofpizza)
+        }
+        else if ($('#size1').val()) {
+            var inputtedsizeofpizza = $('#size1').val() + "" + "price=1000frw";
+            console.log(inputtedsizeofpizza)
+        }
+        else if ($('#size3').val()) {
+            var inputtedsizeofpizza = $('#size3').val() + "" + "price=1000frw";
+            console.log(inputtedsizeofpizza)
+        }
+        if ($('#crust').val()) {
+            var inputtedcrustofpizza = $('#crust').val() + "" + "price=4000frw";
+            console.log(inputtedcrustofpizza)
+        }
+        else if ($('#crust1').val()) {
+            var inputtedcrustofpizza = $('#crust1').val() + "" + "price=4000frw";
+            console.log(inputtedcrustofpizza)
+        }
+        else if ($('#crust3').val()) {
+            var inputtedcrustofpizza = $('#crust3').val() + "" + "price=4000frw";
+            console.log(inputtedcrustofpizza)
+        }
+        else if ($('#quantite').val()) {
+            var inputtedquantityofpizza = $('#quantite').val();
+            console.log(inputtedquantityofpizza)
+        }
+        else if ($('#quantite1').val()) {
+            var inputtedquantityofpizza = $('#quantite1').val();
+            console.log(inputtedquantityofpizza)
+        }
+        else if ($('#quantite3').val()) {
+            var inputtedquantityofpizza = $('#quantite3').val();
+            console.log(inputtedquantityofpizza)
+        }
+        var thechoosentoppings = [];
+
+        $('input[type=checkbox]:checked').each(function () {
+            thechoosentoppings.push($(this).val());
+        });
+        console.log(thechoosentoppings);
+        var additiontoppings = 0;
+        function Toppings(thechoosenToppings) {
+            for (var a = 0; a < theChoosenToppings.length; a++) {
+                if (thechoosenToppings[a] === "Cheese") {
+                    additionToppings += 500
+                    console.log(additionToppings)
+                }
+                else if (theChoosenToppings[a] === "Tomato") {
+                    additionToppings += 600
+                    console.log(additionToppings)
+                }
+                else if (thechoosenToppings[a] === "Aragula") {
+                    additionToppings += 1000
+                    console.log(additionToppings)
+                }
+                else if (theChoosenToppings[a] === "Vegeterian") {
+                    additionToppings += 2000
+                    console.log(additionToppings)
+                }
+                else if (theChoosenToppings[a] === "Mexican") {
+                    additionToppings += 2500
+                    console.log(additionToppings)
+                }
+                else if (theChoosenToppings[a] === "Crackerbread") {
+                    additionToppings += 2500
+                    console.log(additionToppings)
+                }
+                console.log(additionToppings)
+                return additionToppings;
+            }
+            toppings(thechoosentoppings)
+            if ($('#delivery').val() === "yes")
+                var inputtedDeliveryofpizza = $('#delivery1').val() + "" + "delivery charged=250frw";
+            console.log(inputtedDeliveryOfPizza)
+        }
+        if ($('#delivery').val() === "no")
+            var inputtedDeliveryOfPizza = $('#delivery').val();
+        console.log(inputtedDeliveryOfPizza)
+    });
+    var newOrder = new odder(inputtedSizeOfPizza, inputtedQuantityOfPizza, inputtedCrustOfPizza, inputtedDeliveryOfPizza, additionToppings);
+    console.log(newOrder);
+    console.log(theChoosenToppings);
+    $("#view").text(newOrder.sizeOfPizza)
+});
